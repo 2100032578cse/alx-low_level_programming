e527330cdae0cc1079d210adbf5ecda78b7fd900
2100032578cse <uwizeyesandri@gmail.com>
2023-09-07 10:13:10 -0700

    added a space after ,in program

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,11 @@ int main(void)
 	long lg;
 	long long lglg;
 	float f;
-	printf("Size of a char: %lu byte(s)\n",(unsigned long)sizeof(c));
-	printf("Size of an int: %lu byte(s)\n",(unsigned long)sizeof(i));
-	printf("Size of a long int: %lu byte(s)\n",(unsigned long)sizeof(lg));
-	printf("Size of a long long int: %lu byte(s)\n",(unsigned long)sizeof(lglg));
-	printf("Size of a float: %lu byte(s)\n",(unsigned long)sizeof(f));
+
+	printf("Size of a char: %lu byte(s)\n",	(unsigned long)sizeof(c));
+	printf("Size of an int: %lu byte(s)\n",	(unsigned long)sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(lg));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lglg));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
