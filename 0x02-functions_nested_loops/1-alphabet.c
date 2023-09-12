b77a696163b3a359c70e7ce174d91d04abf32f2f
2100032578cse <uwizeyesandri@gmail.com>
2023-09-12 09:36:59 -0700

    function that prints the alphabet, in lowercase, followed by a new

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,17 @@
 #include "main.h"
 
 /**
- * print_alphabet - Entry point
- * Return:0(Success) 
- */
-
+ * print_alphabet - prints lower case alphabets.
+ *
+ * Return: Always 0 (Success)
+*/
 void print_alphabet(void)
 {
 	char c;
 
-	for(c = 'a'; c <= 'z'; c++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
 		_putchar(c);
 	}
 	_putchar('\n');
-	return (0);
 }
