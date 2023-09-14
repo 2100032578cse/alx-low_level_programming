0ba7316decbc24f2ca51ccfbf5c56f3381415dd6
2100032578cse <uwizeyesandri@gmail.com>
2023-09-14 04:05:10 -0700

    prints 0 t0 14 10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,8 +11,9 @@ void more_numbers(void)
 	for (i = 1; i <= 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
-			_putchar(j);
-		_putchar('\n');
+		if (j >= 10)
+			_putchar((j / 10) + '0');
+		_putchar((j % 10) + '0');
 	}
 	_putchar('\n');
 }
