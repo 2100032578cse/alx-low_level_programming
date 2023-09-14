95f101765d662e417bc08c059fa7f607d8c63319
2100032578cse <uwizeyesandri@gmail.com>
2023-09-14 03:56:43 -0700

    prints 0 t0 14 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers from 0 to 14,10 times
+ * Return: nothing
+*/
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+			_putchar(j);
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
