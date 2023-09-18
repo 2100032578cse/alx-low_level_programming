f4e6044bf1f788e8b534565cee60665188e90ac1
2100032578cse <uwizeyesandri@gmail.com>
2023-09-18 00:26:39 -0700

    displaying string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strlen - return string length
+ * @s: string to return length of
+ * Return: return an int,length of s
+*/
+int _strlen(char *s)
+{
+	int len = strlen(s);
+
+	return (len);
+}
