b316e7bfddd3075ea2b083e349b08c872aacee2d
2100032578cse <uwizeyesandri@gmail.com>
2023-09-21 07:50:21 -0700

    used n without new memory for newN

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,16 +7,14 @@
 */
 void print_number(int n)
 {
-/*	unsigned int newN = n;*/
-
 	if (n < 0)
 	{
-		_putchar('-');
-		/*newN = newN * -1;*/
 		n = -n;
+		_putchar('-');
 	}
-
-	if ((n / 10) > 0)
+	if (n / 10)
+	{
 		print_number(n / 10);
-	_putchar((n % 10) + '0');
+	}
+	_putchar(n % 10 + '0');
 }
