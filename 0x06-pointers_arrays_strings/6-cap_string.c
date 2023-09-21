d7fe042cf8c3295d3747f08f6a51dbcabe5b70f1
2100032578cse <uwizeyesandri@gmail.com>
2023-09-21 05:44:56 -0700

    to capitalize eveyr word

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -23,7 +23,7 @@ char *cap_string(char *str)
 				|| str[i] == ')' || str[i] == '.'
 				|| str[i] == '\t' || str[i] == '\n' || str[i] == ' ')
 		{
-			if (str[i + 1] <= 122 && str[i] >= 97)
+			if (str[i + 1] <= 122 && str[i + 1] >= 97)
 			{
 				str[i + 1] = str[i + 1] - 32;
 			}
