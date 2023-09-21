cc4be624eca52774931464533f860d0e1384053c
2100032578cse <uwizeyesandri@gmail.com>
2023-09-21 03:39:13 -0700

    corrected while loop

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,9 +11,9 @@ int _strcmp(char *s1, char *s2)
 	int i, diffans;
 
 	i = 0;
-	while (s1[i] != '\0' && s1[i] ==s2[i])
+	while (s1[i] != '\0' && s1[i] == s2[i])
 	{
-			i++;
+		i++;
 	}
 	diffans = s1[i] - s2[i];
 	return (diffans);
