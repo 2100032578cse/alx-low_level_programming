e77ef497c1b110182256bb07ada5f965115ab8e7
2100032578cse <uwizeyesandri@gmail.com>
2023-09-21 02:57:37 -0700

    comparing s1 ans s2

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+  * _strcmp - will compare s1 ans s2
+  * @s1: string to comare to s2
+  * @s2: string to compare to s1
+  * Return: an int value +ve,0 or _ve.
+*/
+int _strcmp(char *s1, char *s2)
+{
+	int n, i;
+
+	n = 0;
+	while (s1[n] != '\0')
+	{
+		n++;
+	}
+	i = 0;
+	while (s2[i] != '\0')
+	{
+		i++;
+	}
+	if (n > i)
+	{
+		return (15);
+	}
+	else if (n < i)
+	{
+		return (-15);
+	}
+	else
+	{
+		return (0);
+	}
+}
