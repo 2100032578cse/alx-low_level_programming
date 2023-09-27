2a1709b590ea96e3208d0e7c50ec3c596ff7abcd
2100032578cse <uwizeyesandri@gmail.com>
2023-09-27 10:11:01 -0700

    removed i

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,11 +7,9 @@
 */
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
 	if (s[i] == '\0')
 		_putchar('\n');
-	_putchar(s[i]);
+	_putchar(*s);
 	i++;
-	_puts_recursion(s[i]);
+	_puts_recursion(s);
 }
