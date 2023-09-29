cd21b1f6f2220a70df608161528c87db3679879e
2100032578cse <uwizeyesandri@gmail.com>
2023-09-29 11:05:44 -0700

    addition

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * main - multiplies n1 and n2
+ * @argc: number of arguments
+ * @argv: array
+ * Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int i;
+	int j;
+
+	for (i = 1; i < argc; i++)
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			if (isdigit(argv[i][j]) == 0)
+			{
+				printf("Error\n");
+				return (1);
+			}
+			j++;
+		}
+	}
+	i = 1;
+	while (i < argc)
+	{
+		sum = sum + atoi(argv[i]);
+		i++;
+	}
+}
