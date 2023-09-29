4a612bde2d67e44edf4eb7a0f26272bb0beb5423
2100032578cse <uwizeyesandri@gmail.com>
2023-09-29 10:49:16 -0700

    multiply

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,16 +9,16 @@
 */
 int main(int argc, char *argv[])
 {
-	int n1, int n2, int mul;
+	int n1, n2, res;
 
-	if (argc < 3)
+	if (argc <= 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 	n1 = atoi(argv[1]);
 	n2 = atoi(argv[2]);
-	/*mul = n1 * n2;*/
-	printf("%d\n", n1 * n2);
+	result = n1 * n2;
+	printf("%d\n", res);
 	return (0);
 }
