b3c6441abf427e27725e9b623d8c2ed36aa2b5bd
2100032578cse <uwizeyesandri@gmail.com>
2023-09-29 10:25:31 -0700

    multiply n1 and n2

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - multiplies n1 and n2
+ * @argc: number of arguments
+ * @argv: array
+ * Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	int n1 = atoi(argv[1]);
+	int n2 = atoi(argv[2]);
+	int mul;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	mul = n1 * n2;
+	printf("%d\n", mul);
+	return (0);
+}
