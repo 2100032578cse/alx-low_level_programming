90b42eb06e67270d69876677b41d4ef2d6126542
2100032578cse <uwizeyesandri@gmail.com>
2023-10-03 09:21:50 -0700

    cocnting strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,8 +11,6 @@ char *str_concat(char *s1, char *s2)
 	char *conc;
 	int len = 0, i = 0, k = 0, c = 0, c2 = 0, cc = 0;
 
-	if (s1 == NULL && s2 == NULL)
-		return (NULL);
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
