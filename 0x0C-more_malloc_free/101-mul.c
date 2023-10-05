544c7af5767305694d11f6284b7db5654ac7651f
2100032578cse <uwizeyesandri@gmail.com>
2023-10-05 08:13:39 -0700

    Write a program that multiplies two positive numbers.

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,30 @@
+#include  "main.h"
+#include <ctype.h>
+
+/**
+* _atoi - changes a string to an int
+* @s: the string to be changed
+*
+* Return: the converted int
+*/
+int _atoi(char *s)
+{
+
+}
+/**
+ * main - Multiplies two positive numbers.
+ * @argv: The number of arguments
+ * @argc: An array of pointers
+ * Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	char *mult, *rod;
+	int size, index, digit, zeros = 0;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+}
