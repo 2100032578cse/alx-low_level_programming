31c50f23b03e3930b5b3fbe70d253a29d156ced3
2100032578cse <uwizeyesandri@gmail.com>
2023-10-05 06:13:50 -0700

    concatenationof strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,14 +3,15 @@
  * string_nconcat - concatenete two strings
  * @s1: string input
  * @s2: string input
+ * @n: number of chracters from s2
  * Return: a pointer to new allocated memeory
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int len = 0;
+	unsigned int len1 = 0;
 	char *news;
-	int i, k;
-	
+	int i, k, ii, jj, cc = 0;
+
 	if (s2 == NULL)
 		s2 = "";
 	if (s1 == NULL)
@@ -23,16 +24,16 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	k = 0;
 	while (s1[k])
-        {
-                len1++;
-                k;
-        }
-	news = malloc(sizeof(char) *(len1 + n + 1));
+	{
+		len1++;
+		k++;
+	}
+	news = malloc(sizeof(char) * (len1 + n + 1));
 	if (news == NULL)
 		return (NULL);
 	for (ii = 0; ii < len1; ii++)
 	{
-		news[cc++] = s1 [ii];
+		news[cc++] = s1[ii];
 	}
 	for (jj = 0; jj < n; jj++)
 	{
