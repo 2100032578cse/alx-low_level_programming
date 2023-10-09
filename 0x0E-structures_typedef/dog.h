7355c30e88ffde08385562956d54124b6c8b2420
2100032578cse <uwizeyesandri@gmail.com>
2023-10-09 06:07:42 -0700

    defined new type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
-struct dog {
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * struct dog - this construction of dog
+ * @name: elemet name
+ * @age: element age
+ * @owner: element owner
+*/
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
