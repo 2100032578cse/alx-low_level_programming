44a54d08fb16c8567c17901e2bd73f443f8c2025
2100032578cse <uwizeyesandri@gmail.com>
2023-10-19 10:58:23 -0700

    execute before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+void beforemain(void) __attribute__ ((constructor));
+
+/**
+ * beforemain - execute before main
+*/
+void beforemain(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
