b670688230992cc4aed13455f2e607137b87e9d1
2100032578cse <uwizeyesandri@gmail.com>
2023-10-22 23:08:40 -0700

    a func that add ele on end of l list

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ * add_nodeint_end - func to add element at end
+ * @head:head pointer
+ * @n: data to add
+ * Return:  the address of the new element, or NULL if it failed
+*/
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *newn;
+	listint_t *temp = *head;
+
+	newn = malloc(sizeof(listint_t));
+	newn->n = n;
+	newn->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		return (head);
+	}
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+	}
+	temp->next = newn;
+	return (head);
+
+}
