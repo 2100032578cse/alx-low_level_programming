fa436c4bc4635813d3cf8d76e90d56d3cef60a6f
2100032578cse <uwizeyesandri@gmail.com>
2023-10-22 22:29:06 -0700

    a fun that prints number of elements in a linked list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include"lists.h"
+/**
+ *  listint_len - func to calculate nber of eles in list
+ *  @h: head pointer
+ *  Return: number of eles
+*/
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	if (h == NULL)
+		return (0);
+	while (h != NULL)
+	{
+		count++;
+	}
+	return (count);
+}
