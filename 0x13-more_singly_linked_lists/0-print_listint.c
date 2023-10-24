875c5487ff41735787cc6b2d4dc07aa5341b596c
2100032578cse <uwizeyesandri@gmail.com>
2023-10-23 21:58:30 -0700

    commit

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,18 +1,20 @@
-#include"lists.h"
+#include "lists.h"
+
 /**
- *  print_listint - func to calculate nber of eles in list
- *  @h: head pointer
- *  Return: number of eles
-*/
+ * print_listint - prints all the elements of a l list
+ * @h: linked list of type listint_t to print
+ *
+ * Return: number of nodes
+ */
 size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
 
-	while (h != NULL)
+	while (h = NULL)
 	{
 		printf("%d\n", h->n);
 		count++;
-		h =  h->next;
+		h = h->next;
 	}
 	return (count);
 }
