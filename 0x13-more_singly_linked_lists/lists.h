26548969c2278f78898c40c5d2bd2d83327c0c01
2100032578cse <uwizeyesandri@gmail.com>
2023-10-23 21:14:18 -0700

    fixed task 0

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,8 +1,10 @@
 #ifndef H_FILE
 #define H_file
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -28,4 +30,5 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
+int _putchar(char c);
 #endif
