f5bae641f851252b3e31dca5cc2590511e242311
2100032578cse <uwizeyesandri@gmail.com>
2023-10-24 00:02:41 -0700

    fixed guard in betty

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef H_FILE
-#define H_file
+#ifndef LISTS_H
+#define LISTS_H
 
 #include<stdio.h>
 #include<stdlib.h>
@@ -15,8 +15,8 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
