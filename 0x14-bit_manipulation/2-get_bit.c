42d0699a8d84599ff401325e5c1e44b593db8920
2100032578cse <uwizeyesandri@gmail.com>
2023-10-26 03:31:29 -0700

    func to get bit on index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*get_bit - returns a bit at the given index
+*@n: the number
+*@index: index for bits
+*Return: bit got
+*/
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index <= 62)
+	{
+		return (((n >> index) & 1));
+	}
+	else
+		return (1);
+}
