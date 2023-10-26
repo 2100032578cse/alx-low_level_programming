be87bd641e070bb8c0b9f5f3a94cc11b35e9143b
2100032578cse <uwizeyesandri@gmail.com>
2023-10-26 03:46:31 -0700

    fixed a file

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,11 +10,8 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (n != NULL || (index <= (sizeof(unsigned long int) * 8) - 1))
-	{
-		*n |= (1 << index);
-		return (1);
-	}
-	else
+	if (index > 63)
 		return (-1);
+	*n = (*n | (1 << index));
+	return (1);
 }
