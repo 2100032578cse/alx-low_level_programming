f75c5a2c5cfa16448df960e71bcfa0621a8b6833
2100032578cse <uwizeyesandri@gmail.com>
2023-10-30 10:06:28 -0700

    added guard on main

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,9 +1,12 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 #include<stdio.h>
-#include<stlib.h>
+#include<stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
