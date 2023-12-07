8e8f5b9051e81b7beff4264d623519e1cfbb20a9
2100032578cse <uwizeyesandri@gmail.com>
2023-12-07 08:22:02 -0800

    add all eles in d;ist

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * sum_dlistint - func to add list eles
+ * @head: the head pointer
+ * Return: sum
+*/
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+	while (head->next != NULL)
+	{
+		sum = sum + head->n;
+	}
+	sum = sum + head->next;
+	return (sum);
+}
