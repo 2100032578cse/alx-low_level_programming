51a2410ed304910537cef467396d037331301f04
2100032578cse <uwizeyesandri@gmail.com>
2023-12-07 05:03:51 -0800

    func to print linked list eles

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ *  print_dlistint - print the elements of dlinked list
+ *  @h: head pointer
+ *  Return: the number of nodes
+*/
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t ans = 0;
+
+	if (h == NULL)
+		return (0);
+	while (h)
+	{
+		ans++;
+		printf("%d", h->n);
+		h = h->next;
+	}
+	return (ans);
+}
