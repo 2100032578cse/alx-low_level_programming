2f586ad074f92096776c4287c2037aec70adbe40
2100032578cse <uwizeyesandri@gmail.com>
2023-12-07 05:10:00 -0800

    count nodes

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ *  dlistint_len - print the elements of dlinked list
+ *  @h: head pointer
+ *  Return: the number of nodes
+*/
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t ans = 0;
+
+	if (h == NULL)
+		return (0);
+	while (h)
+	{
+		ans++;
+		h = h->next;
+	}
+	return (ans);
+}
