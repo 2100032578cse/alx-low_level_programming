f2224a78b430a44879f5cca7ab5a70175466859e
2100032578cse <uwizeyesandri@gmail.com>
2023-12-13 02:52:02 -0800

    task for opc of c to python

---
+++ 0x18-dynamic_libraries/ops.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+/**
+ * add -  add 2 nums
+ * @a: first num
+ * @b: second num
+ * Return: result after operation
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * mul -  multiply 2 nums
+ * @a: first num
+ * @b: second num
+ * Return: result after operation
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * sub -  subtract 2 nums
+ * @a: first num
+ * @b: second num
+ * Return: result after operation
+*/
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * div - divide 2 nums
+ * @a: first num
+ * @b: second num
+ * Return: result after operation
+*/
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod -  give modulus of 2 nums
+ * @a: first num
+ * @b: second num
+ * Return: result after operation
+*/
+int mod(int a, int b)
+{
+	return (a % b);
+}
