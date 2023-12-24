9991a56e4d5698a6da154ff09d4a1a0a0e18ecd9
Sandrine <99196826+2100032578cse@users.noreply.github.com>
2023-12-24 22:40:26 +0530

    Update 4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -7,14 +7,10 @@
 */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	hash_node_t *node;
 	unsigned long int index;
-
-	if (ht == NULL)
-		return (NULL);
-	if (hkey == NULL)
-		return (NULL);
-	if (*key == '\0')
+	hash_node_t *node;
+	
+	if (ht == NULL || key == NULL || *key == '\0')
 		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
 	if (index >= ht->size)
@@ -22,12 +18,5 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	node = ht->array[index];
 	while (node && strcmp(node->key, key) != 0)
 		node = node->next;
-	if (node == NULL)
-	{
-		return (NULL);
-	}
-	else
-	{
-		return (node->value);
-	}
+	return ((node == NULL) ? NULL : node->value);
 }
