c8ebbd9fc032cf9e788740830905edd428c0b7aa
2100032578cse <uwizeyesandri@gmail.com>
2023-12-24 08:24:22 -0800

    fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,4 +1,4 @@
-# include "hash_tables.h"
+#include "hash_tables.h"
 /**
  * hash_table_create - a func to crate a table
  * @size: the size of table
@@ -9,15 +9,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *ht;
 	unsigned long int i;
 	ht = malloc(sizeof(hash_table_t));
-
 	if (ht == NULL)
 		return (NULL);
-
 	ht->array = malloc(sizeof(hash_node_t *) * size);
-
 	if (ht->array == NULL)
 		return (NULL);
-
 	for (i = 0; i < size; i++)
 	{
 		ht->array[i] = NULL;
