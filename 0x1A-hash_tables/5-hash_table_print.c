fb54c5d87d87e454e579c9fc453f04eef8b8d0ca
2100032578cse <uwizeyesandri@gmail.com>
2023-12-24 09:19:40 -0800

    task 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - Prints a hash table.
+ * @ht: A pointer to the hash table to print.
+ * Return: nothin'
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	unsigned char flag = 0;
+	hash_node_t *node;
+
+	if (ht == NULL)
+		return;
+	printf("{");
+	i = 0;
+	while (i < ht->size)
+	{
+		if (ht->array[i] != NULL)
+		{
+			if (flag == 1)
+				printf(", ");
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				printf("'%s': '%s'", node->key, node->value);
+				node = node->next;
+				if (node != NULL)
+					printf(", ");
+			}
+			flag = 1;
+		}
+		i++;
+	}
+	printf("}\n");
+}
