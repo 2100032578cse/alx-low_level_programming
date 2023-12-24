0584eb148e2fcd6ababcb259cf2b8e231b01af06
Sandrine <99196826+2100032578cse@users.noreply.github.com>
2023-12-24 22:19:47 +0530

    Update test.c

--- 0x1A-hash_tables/test.c
+++ 0x1A-hash_tables/test.c
@@ -1,51 +1,20 @@
-#include "hash_tables.h"
-/**
- * hash_table_set - add element to the hash table.
- * @ht: hash_table_t pointer
- * @key: the key
- * @value: the values required
- * Return: 1 if it succeeded, 0 otherwise
- */
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	hash_node_t *new;
-	char *value_copy;
+	hash_node_t *node;
 	unsigned long int index;
-  unsigned long int i;
 
-	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
-		return (0);
-
-	value_copy = strdup(value);
-	if (value_copy == NULL)
-		return (0);
+	if (ht == NULL)
+		return (NULL);
+	if (hkey == NULL)
+		return (NULL);
+	if (*key == '\0')
+		return (NULL);
 
 	index = key_index((const unsigned char *)key, ht->size);
-	for (i = index; ht->array[i]; i++)
-	{
-		if (strcmp(ht->array[i]->key, key) == 0)
-		{
-			free(ht->array[i]->value);
-			ht->array[i]->value = value_copy;
-			return (1);
-		}
-	}
-
-	new = malloc(sizeof(hash_node_t));
-	if (new == NULL)
-	{
-		free(value_copy);
-		return (0);
-	}
-	new->key = strdup(key);
-	if (new->key == NULL)
-	{
-		free(new);
-		return (0);
-	}
-	new->value = value_copy;
-	new->next = ht->array[index];
-	ht->array[index] = new;
-
-	return (1);
+	if (index >= ht->size)
+		return (NULL);
+	node = ht->array[index];
+	while (node && strcmp(node->key, key) != 0)
+		node = node->next;
+	return ((node == NULL) ? NULL : node->value);
 }
