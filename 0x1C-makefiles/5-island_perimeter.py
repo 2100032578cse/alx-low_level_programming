a54924f88900218549f70b86c40db0bf82912348
2100032578cse <uwizeyesandri@gmail.com>
2024-02-29 07:54:40 -0800

    grid

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" definition of the function"""
+
+
+def island_perimeter(grid):
+    """ this will return parameter of islandd"""
+
+    c = 0
+    length = len(grid)
+    width = len(grid[0])
+    size = 0
+
+    for i in range(length):
+        for j in range(width):
+            if grid[i][j] == 1:
+                    size = size + 1
+                    if(j >0 and grid[i][j - 1] == 1):
+                        c = c + 1
+                    if(i > 0 and grid[i - 1][j] == 1):
+                        c = c + 1
+    return size * 4 - c * 2
